a0b9f27bebd94ae45df7a431de09bf0f0108077e
Urban Dragonfly <Urban_Dragonfly@outlook.com>
2024-10-16 18:44:47 +0200

    added code to display number of values in zakupy dictionary

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ print("Lista zakupów: ")
 print()
 for k, v in zakupy.items():
     print(f"{k.capitalize()} to sklep gdzie znajdę nastepujące produkty: {', '.join(v.capitalize() for v in v)}")
+
+print()
+x = sum(map(len, zakupy.values()))
+print(f"Lista zakupów zawiera {x} produktów.")
