ee9fbae0ef127f7a160b4dbd5eaebfd599838d52
Urban Dragonfly <Urban_Dragonfly@outlook.com>
2024-10-16 18:42:51 +0200

    added code to display shopping list to main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ sys.stdout.reconfigure(encoding='utf-8')
 
 zakupy = {"putka" : ["chleb", "pączek", "bułki"],
           "warzywniak" : ["marchew", "seler", "rukola"],}
+
+print("Lista zakupów: ")
+print()
+for k, v in zakupy.items():
+    print(f"{k.capitalize()} to sklep gdzie znajdę nastepujące produkty: {', '.join(v.capitalize() for v in v)}")
